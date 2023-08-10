65a7814f4e75a5aada4965f100f66fecf803fbe6
17Sergey <77524020+17Sergey@users.noreply.github.com>
2023-08-10 18:26:38 +0300

    Added users data for request

---
+++ data.js
@@ -0,0 +1,10 @@
+export default {
+    users: [
+        { id: 1, isFriend: false, fullname: "Sergey Kachaliuk", avatarUrl: "https://cdn3.iconfinder.com/data/icons/avatars-9/145/Avatar_Penguin-512.png", status: "Let your dreams come true", location: { city: "Brest", country: "Belarus" } },
+        { id: 2, isFriend: true, fullname: "Nikolay Ilyin", avatarUrl: "https://cdn-icons-png.flaticon.com/512/3382/3382653.png", status: "Gonna cry?", location: { city: "Grodno", country: "Belarus" } },
+        { id: 3, isFriend: false, fullname: "Maxim Buchatsky", avatarUrl: "https://cdn3.iconfinder.com/data/icons/animals-1-1/512/12-512.png", status: "Sertified overthinker", location: { city: "Minsk", country: "Belarus" } },
+        { id: 4, isFriend: true, fullname: "Pavel Pyasetskiy", avatarUrl: "https://m.covest.pro/images/copyprofile/49.png", status: "Heart of a lion", location: { city: "Minsk", country: "Belarus" } },
+        { id: 5, isFriend: false, fullname: "Dominic Toretto", avatarUrl: "https://cdn-icons-png.flaticon.com/512/3382/3382653.png", status: "The most important thing in life will always be family", location: { city: "Los-Angeles", country: "USA" } },
+        { id: 6, isFriend: true, fullname: "Scott Mccall", avatarUrl: "https://cdn-icons-png.flaticon.com/512/3382/3382653.png", status: "Because we don't run", location: { city: "Los-Angeles", country: "USA" } },
+    ]
+}
